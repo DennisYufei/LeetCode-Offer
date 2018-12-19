01ada9c7bae30749873ed1b0bdd14f8766e29115
Dennis <604484252@qq.com>
2018-12-19 20:41:18 +0800

    12.19 leetcode 27

--- src/Leetcode/array/RemoveElement.java
+++ src/Leetcode/array/RemoveElement.java
@@ -2,18 +2,22 @@ package Leetcode.array;
 
 /**
  * 给定一个数组和一个值，删除数组中该有和该值相等的元素，并返回新长度。要求空间复杂度为O(1)
- *
+ * <p/>
  * Created by Dennis on 2018/12/19.
  */
 public class RemoveElement {
 
     /**
      * 双指针法1，时间复杂度为O(n)，空间复杂度为O(1)
+     *
      * @param nums 输入数组
-     * @param val 目标值
+     * @param val  目标值
      * @return int
      */
     public static int remove(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
         int i = 0;
         for (int j = 0; j < nums.length; j++) {
             if (nums[j] != val) {
