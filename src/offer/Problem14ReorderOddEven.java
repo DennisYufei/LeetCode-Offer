05746c7aae689c655a5e9c9aee60ea88a6a51f8f
Dennis <604484252@qq.com>
2019-07-16 10:09:26 +0800

    奇数放在偶数前面

--- src/offer/Problem14ReorderOddEven.java
+++ src/offer/Problem14ReorderOddEven.java
@@ -1,8 +1,11 @@
 package offer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 调整数组顺序使得奇数放在偶数前面，双指针法,一个指针指向头一个指针指向末尾，然后奇数和偶数对换位置
- *
+ * 思路1：调整数组顺序使得奇数放在偶数前面，双指针法,一个指针指向头一个指针指向末尾，然后奇数和偶数对换位置
+ * 思路2：空间换时间，用2个ArrayList，一个装奇数，一个装偶数，然后合并两个ArrayList，然后再返回数组
  * @author Dennis
  * @date 2018/4/16
  */
@@ -13,23 +16,39 @@ public class Problem14ReorderOddEven {
     }
 
     public void order(int[] array) {
-        if (array == null && array.length == 0) {
-            return;
+//        if (array == null && array.length == 0) {
+//            return;
+//        }
+//        int start = 0;
+//        int end = array.length - 1;
+//        while (start < end) {
+//            while (start < end && !isEven(array[start])) {
+//                start++;
+//            }
+//            while (start < end && isEven(array[end])) {
+//                end--;
+//            }
+//            if(start<end){
+//                int temp = array[start];
+//                array[start] = array[end];
+//                array[end] = temp;
+//            }
+//        }
+        if(array == null){
+            throw new IllegalArgumentException("数组为空");
         }
-        int start = 0;
-        int end = array.length - 1;
-        while (start < end) {
-            while (start < end && !isEven(array[start])) {
-                start++;
+        List<Integer> list1 = new ArrayList<Integer>();
+        List<Integer> list2 = new ArrayList<Integer>();
+        for(int i = 0;i < array.length;i++){
+            if(array[i] % 2 == 1){
+                list1.add(array[i]);
+            }else{
+                list2.add(array[i]);
             }
-            while (start < end && isEven(array[end])) {
-                end--;
-            }
-            if(start<end){
-                int temp = array[start];
-                array[start] = array[end];
-                array[end] = temp;
-            }
+        }
+        list1.addAll(list2);
+        for(int i = 0;i<array.length;i++){
+            array[i] = list1.get(i);
         }
     }
 
